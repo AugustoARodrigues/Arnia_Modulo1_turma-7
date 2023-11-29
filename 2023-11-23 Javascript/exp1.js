b7e37bcb8d6c6089c289219b71e7546f9ba6df42
AugustoARodrigues <augustomedaar@gmail.com>
2023-11-29 14:26:10 -0300

    add

---
+++ 2023-11-23 Javascript/exp1.js
@@ -0,0 +1,92 @@
+// ex 1:
+
+// console.log('Oi pessoal da turma 7')
+
+
+// ex 2:
+// let frase = 'Alo alunos da turma 7'
+
+// console.log(frase)
+
+// ex 3:
+// let frase = 'Alo alunos da turma 7'
+
+// console.log(frase)
+
+// const frase2 = 'teste'
+
+// ex 4:
+// let frase = 'Alo alunos da turma 7'
+// frase ='Alo turmaaaaaa'
+
+// console.log(frase)
+
+// const frase2 = 'teste'
+
+// ex 5:
+// let numero = 1
+// console.log(numero)
+
+
+// ex 6:
+// let numeroDecimal = 1.5
+// console.log(numeroDecimal)
+
+// ex 7:
+//somar 2 numeros 
+// let soma = 2 + 2
+// console.log(soma)
+
+
+// ex 8:
+//subtracao 2 numeros 
+// let subtracao = 10 - 5
+// console.log(subtracao)
+
+
+// ex 9:
+//multiplicacao
+// let multiplicacao = 2 * 5
+// console.log(multiplicacao)
+
+// ex 10:
+//divisao
+// let divisao = 30 / 2
+// console.log(divisao)
+
+
+//ex 11:
+//potenciação (5 elevado à 2)
+// let potenciacao = 5 ** 2
+// console.log(potenciacao)
+
+
+//ex 12:
+//resto de uma divisão
+// let resto = 9 % 2
+// console.log(resto)
+
+
+//ex 13:
+// let soma3 = 3 + 8 + 9
+// console.log(soma3)
+
+
+//ex 14:
+// let somaTiposDiferentes = 3 + 'Arnia'
+// console.log(somaTiposDiferentes)
+
+
+//ex 15:
+// let somaNumeros = '1' + '1'
+// console.log(somaNumeros)
+
+
+//ex 16:
+// let soma4 = 2 + '2'
+// console.log(soma4)
+
+
+//ex 17:
+let nomeCompleto = 'Letícia ' + 'Porto ' + 'Soares'
+console.log(nomeCompleto)
