534e1956bdf1c9ae927071ccbbef117cce72ff49
AugustoARodrigues <augustomedaar@gmail.com>
2023-12-02 12:30:53 -0300

    add

---
+++ 2023-11-27/exp1.js
@@ -0,0 +1,8 @@
+const io = require('../io/io')
+/* Faça um programa que receba dois números e imprima a diferença de um pelo outro */
+
+io.write('Digite o primeiro numero: ')
+const numero1 = io.readInt()
+
+io.write(`O numero 1 digitado foi:  ${numero1}`)
+
